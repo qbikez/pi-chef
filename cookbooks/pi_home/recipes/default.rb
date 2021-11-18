360f93e6ac6e58a68833c9c3998917a85d1c4a4b
jakub.pawlowski <jakub.pawlowski@guestline.com>
2021-11-18 06:50:35 +0100

    fix default

--- cookbooks/pi_home/recipes/default.rb
+++ cookbooks/pi_home/recipes/default.rb
@@ -2,6 +2,9 @@
 include_recipe '::net_openvpn'
 include_recipe '::net_tailscale'
 include_recipe '::gitconfig'
-include_recipe '::vsts_agent'
+
+# don't touch vsts_agent when running from a pipeline...
+# include_recipe '::vsts_agent'
+
 include_recipe '::svc_home_assistant'
 include_recipe '::svc_other'
